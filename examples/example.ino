3aec936df777479f7e5788c5b24df2077f72c221
Peter Buelow <goballstate@gmail.com>
2020-05-27 05:56:32 -0500

    Fixing up the example as it wasn't correct to the latest

--- examples/example.ino
+++ examples/example.ino
@@ -13,18 +13,18 @@ SparkTime rtc;
 
 void setup()
 {
-	Serial.begin(9600);
-	/* Get our time sync started */
-	rtc.begin(&UDPClient, "pool.ntp.org");
-	rtc.setTimeZone(CST_OFFSET);
-	/* Set our position and a default timezone value */
-	sun.setPosition(LATITUDE, LONGITUDE, DST_OFFSET);
+    Serial.begin(9600);
+    /* Get our time sync started */
+    rtc.begin(&UDPClient, "pool.ntp.org");
+    rtc.setTimeZone(CST_OFFSET);
+    /* Set our position and a default timezone value */
+    sun.setPosition(LATITUDE, LONGITUDE, DST_OFFSET);
 }
 
 void loop()
 {
-	double sunrise;
-	double sunset;
+    double sunrise;
+    double sunset;
     double civilsunrise;
     double civilsunset;
     double astrosunrise;
@@ -32,38 +32,38 @@ void loop()
     double nauticalsunrise;
     double nauticalsunset;
 
-	/* Check to see if we have a valid time sync yet, if not, just wait a bit and loop */
-	if (!rtc.hasSynced()) {
-		Particle.publish("No time sync yet");
-		delay(5000);
-		return;
-	}
-	else {
-		/* Get the current time, and set the Sunrise code to use the current date */
-		currentTime = rtc.now();
-		sun.setCurrentDate(rtc.year(currentTime), rtc.month(currentTime), rtc.day(currentTime));
-		/* Check to see if we need to update our timezone value */
-		if (rtc.isUSDST(currentTime))
-			sun.setTZOffset(DST_OFFSET);
-		else
-			sun.setTZOffset(CST_OFFSET);
-	}
+    /* Check to see if we have a valid time sync yet, if not, just wait a bit and loop */
+    if (!rtc.hasSynced()) {
+    	Particle.publish("No time sync yet");
+    	delay(5000);
+    	return;
+    }
+    else {
+    	/* Get the current time, and set the Sunrise code to use the current date */
+    	currentTime = rtc.now();
+    	sun.setCurrentDate(rtc.year(currentTime), rtc.month(currentTime), rtc.day(currentTime));
+    	/* Check to see if we need to update our timezone value */
+    	if (rtc.isUSDST(currentTime))
+    		sun.setTZOffset(DST_OFFSET);
+    	else
+    		sun.setTZOffset(CST_OFFSET);
+    }
 
-	sunrise = sun.calcSunriseLocal();
-	sunset = sun.calcSunsetLocal();
-	civilsunrise = sun.calcCivlSunriseLocal();
-	civilsunset = sun.calcCivilSunsetLocal();
-	nauticalsunrise = sun.calcNauticalSunriseLocal();
-	nauticalsunset = sun.calcNauticalSunsetLocal();
-	astrosunrise = sun.calcAstronomicalSunriseLocal();
-	astrosunset = sun.calcAstronomicalSunsetLocal();
+    sunrise = sun.calcSunrise();
+    sunset = sun.calcSunset();
+    civilsunrise = sun.calcCivilSunrise();
+    civilsunset = sun.calcCivilSunset();
+    nauticalsunrise = sun.calcNauticalSunrise();
+    nauticalsunset = sun.calcNauticalSunset();
+    astrosunrise = sun.calcAstronomicalSunrise();
+    astrosunset = sun.calcAstronomicalSunset();
 
-	Serial.print("Sunrise is ");
-	Serial.print(sunrise);
-	Serial.println(" minutes past midnight.");
-	Serial.print("Sunset is ");
-	Serial.print(sunset);
-	Serial.print(" minutes past midnight.");
-	delay(1000);
+    Serial.print("Sunrise is ");
+    Serial.print(sunrise);
+    Serial.println(" minutes past midnight.");
+    Serial.print("Sunset is ");
+    Serial.print(sunset);
+    Serial.print(" minutes past midnight.");
+    delay(1000);
 }
 
