1c10d9851b9ceb4d7c126c34acf421438cb1b826
Peter Buelow <goballstate@gmail.com>
2020-09-21 09:21:00 -0500

    New release updates
    * Fixed the examples, now there is a Particle specific and ESP32 specific example
    * Fixing library.properties to exclude AVR so it can be used in the Arduino IDE

---
+++ examples/particle/example.ino
@@ -0,0 +1,48 @@
+#include <sunset.h>
+
+/* I'm in the midwest, so this is what I use for my home */
+#define LATITUDE        41.12345
+#define LONGITUDE       -87.98765
+#define DST_OFFSET      -5
+
+SunSet sun;
+SerialLogHandler logHandler;
+
+void setup()
+{
+    sun.setPosition(LATITUDE, LONGITUDE, DST_OFFSET);
+}
+
+void loop()
+{
+    static int currentDay = 32;
+    int sunrise;
+    int sunset;
+    double civilsunrise;
+    double civilsunset;
+    double astrosunrise;
+    double astrosunset;
+    double nauticalsunrise;
+    double nauticalsunset;
+    
+    if (currentDay != Time.day()) {
+        Particle.syncTime();
+        waitUntil(Particle.syncTimeDone);
+        sun.setCurrentDate(Time.year(), Time.month(), Time.day());
+        currentDay = Time.day();
+    }
+    sunrise = static_cast<int>(sun.calcSunrise());
+    sunset = static_cast<int>(sun.calcSunset());
+    civilsunrise = sun.calcCivilSunrise();
+    civilsunset = sun.calcCivilSunset();
+    nauticalsunrise = sun.calcNauticalSunrise();
+    nauticalsunset = sun.calcNauticalSunset();
+    astrosunrise = sun.calcAstronomicalSunrise();
+    astrosunset = sun.calcAstronomicalSunset();
+    
+    Log.info("Sunrise at %d:%dam, Sunset at %d:%dpm", (sunrise/60), (sunrise%60), (sunset/60), (sunset%60));
+    Log.info("Civil Sunrise at %f mpm, Sunset at %f mpm", civilsunrise, civilsunset);
+    Log.info("Nautical Sunrise at %f mpm, Sunset at %f mpm", nauticalsunrise, nauticalsunset);
+    Log.info("Astronomical Sunrise at %f mpm, Sunset at %f mpm", astrosunrise, astrosunset);
+    delay(1000);
+}
