1633bedac786de6e0151c0fd7d91a87cf557ecf4
Peter Buelow <goballstate@gmail.com>
2020-10-12 11:51:01 -0500

    Fixing missing html

---
+++ html/search/variables_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['sunset_5fastonomical_42',['SUNSET_ASTONOMICAL',['../classSunSet.html#a8b915a3546b9a3ed4b183e441b167e68',1,'SunSet']]],
+  ['sunset_5fcivil_43',['SUNSET_CIVIL',['../classSunSet.html#ad6b44cf813b73a4e5294bbeb470541a8',1,'SunSet']]],
+  ['sunset_5fnautical_44',['SUNSET_NAUTICAL',['../classSunSet.html#a1a233aa8dd518db5e2e478541840a8c0',1,'SunSet']]],
+  ['sunset_5fofficial_45',['SUNSET_OFFICIAL',['../classSunSet.html#af459326264de32d4cdb1d1e981642e05',1,'SunSet']]]
+];
